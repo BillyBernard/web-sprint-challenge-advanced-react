f2b62a895784a167cd6d3e5d7a737a0ef59a5c18
BillyBernard <billybernard54@gmail.com>
2021-10-01 10:45:54 -0600

    finished testing and fixed component code

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,19 +2,14 @@
 import { useState } from 'react';
 
 const useForm = (initialValue) => {
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [values, setValues] = useState(initialValue);
 
 const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
 
-  return([values, showSuccessMessage, handleSubmit, handleChanges]);
+  return([values, handleChanges]);
 }
 
 export default useForm;
